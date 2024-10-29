2b72414611d9f82a3e50d3e5cc0d2ceae6edeb48
Terry Seanard <tseanard@stellix.com>
2024-10-29 19:15:38 -0400

    Added sudo version of chmods in _runMonitor.sh so that it works across variations of Ubuntu (EC2, ProxMox)

--- _runMonitor.sh
+++ _runMonitor.sh
@@ -3,4 +3,7 @@ export CADVISOR_VOLUME_ROOTFS=/
 chmod -R 777 ./grafana_data
 chmod -R 777 ./prom
 chmod -R 777 ./prometheus_data
+sudo chmod -R 777 ./grafana_data
+sudo chmod -R 777 ./prom
+sudo chmod -R 777 ./prometheus_data
 docker-compose up -d
